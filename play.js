764bfb29c6c340aa61e49f780c0ff7f2563b0f1b
Sahil Athia <sahilathia918@gmail.com>
2020-11-04 17:51:17 +0000

    added standard input event

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
+const { stdin } = require('process');
 const { connect } = require('./client')
 console.log('Connecting ...');
 connect();
+
+const handleUserInpur = function() {
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit()
+    }
+  })
+}
+
+const setUpInput = function() {
+  const stdin = process.stdin
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8')
+  stdin.resume()
+  return stdin
+}
+handleUserInpur()
+setUpInput()
+
+
